6061d5587e382d190c062770122afd041dffa512
Kostas Georgiadis <kostas.georgiadis@engagetech.com>
2018-07-10 22:55:59 +0300

    Add hashcode and equals to NumberExp

--- jisp-java/src/main/java/jisp/NumberExp.java
+++ jisp-java/src/main/java/jisp/NumberExp.java
@@ -1,11 +1,26 @@
 package jisp;
 
+import java.util.Objects;
+
 public class NumberExp extends JispExp {
 
     private final int val;
 
     public NumberExp(int val) {
         this.val = val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NumberExp numberExp = (NumberExp) o;
+        return val == numberExp.val;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val);
     }
 
     @Override
